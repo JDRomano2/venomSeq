6ffb077b138fcd3d26585bf1a72bbb97f6762c40
Joseph D. Romano <jdromano2@gmail.com>
2019-02-18 10:30:56 -0500

    Add metadata to setup.py

--- setup.py
+++ setup.py
@@ -11,14 +11,26 @@ if PRERELEASE:
 else:
   VERSION = '%d.%d' % (MAJOR, MINOR)
 
+with open("README.md", "r") as fp:
+  long_description = fp.read()
 
 setup(
   name='VenomSeq',
   version=VERSION,
   description='Python code for performing the data analysis in the VenomSeq workflow',
+  long_description=long_description,
   author='Joseph D. Romano',
   author_email='jdromano2@gmail.com',
-  url='https://jdr.bio/research',
+  url='https://github.com/jdromano2/venomseq',
   packages=['venomseq'],
-  package_dir={'venomseq': 'venomseq'}
+  package_dir={'venomseq': 'venomseq'},
+  classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Environment :: Console",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Programming Language :: Python :: 3.6",
+    "Topic :: Scientific/Engineering :: Bio-Informatics"
+  ]
 )
